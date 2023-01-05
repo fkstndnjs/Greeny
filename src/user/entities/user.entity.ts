7d3fb0290ef277d8cd678a9a3e89a98a5d55ddda
YuSeokHyun <fkstndnjs@naver.com>
2023-01-05 10:43:39 +0900

    docs: user Entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../common/entity/baseEntity';
+
+@Entity('user')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @Column({
+    length: 30,
+  })
+  name: string;
+
+  @Column({
+    length: 100,
+  })
+  email: string;
+
+  @Column({
+    length: 100,
+  })
+  password: string;
+
+  @Column({ length: 60 })
+  signupVerifyToken: string;
+}
