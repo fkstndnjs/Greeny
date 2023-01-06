fad7412db9bcdc0084d83061be4ce7959bfc918c
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 17:02:35 +0900

    feat: add interceptor

---
+++ src/common/interceptor/success/success.interceptor.ts
@@ -0,0 +1,18 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class SuccessInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        return { success: true, data };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as expressBasicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
+import { SuccessInterceptor } from './common/interceptor/success/success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new SuccessInterceptor());
 
   app.use(
     ['/swagger-api'],
