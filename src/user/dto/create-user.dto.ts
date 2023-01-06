ae64c83d029d4714acc92d2506fdaa97e8dee338
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 11:34:07 +0900

    docs: create dto for signup

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(16)
+  password: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(10)
+  nickname: string;
+}
