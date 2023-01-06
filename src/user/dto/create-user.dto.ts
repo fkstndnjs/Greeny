22e20e0c917b25194d948b1d5a854158b357f321
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 15:14:40 +0900

    chore: change validation error message

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,10 +3,10 @@ import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @MinLength(2, {
-    message: '최소 2글자 이상 작성해주세요',
+    message: '이름은 최소 2글자 이상 작성해주세요',
   })
   @MaxLength(10, {
-    message: '최대 10글자까지 가능합니다',
+    message: '이름은 최대 10글자까지 가능합니다',
   })
   name: string;
 
@@ -20,19 +20,19 @@ export class CreateUserDto {
 
   @IsString()
   @MinLength(10, {
-    message: '최소 10글자 이상 작성해주세요',
+    message: '비밀번호는 최소 10글자 이상 작성해주세요',
   })
   @MaxLength(16, {
-    message: '최대 16글자까지 가능합니다',
+    message: '비밀번호는 최대 16글자까지 가능합니다',
   })
   password: string;
 
   @IsString()
   @MinLength(1, {
-    message: '최소 1글자 이상 작성해주세요',
+    message: '닉네임은 최소 1글자 이상 작성해주세요',
   })
   @MaxLength(10, {
-    message: '최대 10글자까지 가능합니다',
+    message: '닉네임은 최대 10글자까지 가능합니다',
   })
   nickname: string;
 }
