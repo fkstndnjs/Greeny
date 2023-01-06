28c636d812e0116bf6c9736e3b36b63c42688e71
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 16:38:35 +0900

    fix: jwt

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../../user/user.service';
 
 @Injectable()
@@ -9,17 +9,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
-  async validate(payload) {
+  async validate(payload: { id: number }) {
     const users = await this.userService.findById(payload.id);
 
     if (!users) {
-      throw new BadRequestException('뭐야');
+      throw new UnauthorizedException('유저 정보가 존재하지 않습니다');
     }
 
-    return users;
+    return { id: users.id };
   }
 }
