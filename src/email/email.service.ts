3137b2a988290b6a087deaa3dce2020e5093f809
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 12:03:38 +0900

    feat: sendSignUpMail

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,7 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import Mail from 'nodemailer/lib/mailer';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
   private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  async sendSignUpMail(email: string, signupVerifyToken: string) {
+    const baseUrl = process.env.BASE_URL;
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    const mailOptions = {
+      to: email,
+      from: 'Greeny',
+      sender: 'Greeny',
+      subject: '테스트 가입 인증 메일',
+      html: `<form action="${url}" method="POST">
+    <button>가입확인</button>
+        </form>
+    `,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
 }
