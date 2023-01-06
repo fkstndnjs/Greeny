2dd858c655169785d4f4f7044bd80d6b0acb5005
YuSeokHyun <fkstndnjs@naver.com>
2023-01-06 11:36:15 +0900

    chore: add userRepository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  createUser(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto) {}
 }
