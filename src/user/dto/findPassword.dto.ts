269300f7971cc23fb40e0e0dc78c01fd418f86f7
fkstndnjs <fkstndnjs@naver.com>
2023-01-08 22:23:50 +0900

    docs: findPasswordDto

---
+++ src/user/dto/findPassword.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class FindPasswordDto {
+  @IsString()
+  name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: '이메일 항목이 이메일 형식이 아닙니다',
+    },
+  )
+  email: string;
+
+  @IsString()
+  userId: string;
+}
