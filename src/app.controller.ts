c418d07e1a1d0899434937823c4d628546a15f81
YuSeokHyun <fkstndnjs@naver.com>
2023-01-10 11:36:57 +0900

    feat; upload file to S3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,12 +13,16 @@ import { JwtAuthGuard } from './auth/jwt/jwt.guard';
 import { CurrentUser } from './common/decorator/currentUser';
 import { User } from './user/entities/user.entity';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AwsService } from './aws/aws.service';
 
 @ApiTags('app')
 @UseGuards(JwtAuthGuard)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly awsService: AwsService,
+  ) {}
 
   @Get()
   getHello(@CurrentUser() user: User): string {
@@ -28,6 +32,6 @@ export class AppController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    return await this.awsService.uploadFileToS3('test', file);
   }
 }
--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as AWS from 'aws-sdk';
+import * as path from 'path';
 @Injectable()
-export class AwsService {}
+export class AwsService {
+  private readonly S3: AWS.S3;
+  public readonly S3_BUCKET_NAME: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.S3 = new AWS.S3({
+      accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'),
+      secretAccessKey: this.configService.get('AWS_S3_SECRET_KEY'),
+      region: this.configService.get('AWS_S3_REGION'),
+    });
+    this.S3_BUCKET_NAME = this.configService.get('AWS_S3_BUCKET_NAME');
+  }
+
+  async uploadFileToS3(folder: string, file: Express.Multer.File) {
+    try {
+      const key = `${folder}/${Date.now()}_${path.basename(
+        file.originalname,
+      )}`.replace(/ /g, '');
+
+      const s3Object = await this.S3.putObject({
+        Bucket: this.S3_BUCKET_NAME,
+        Key: key,
+        Body: file.buffer,
+        ACL: 'public-read',
+        ContentType: file.mimetype,
+      }).promise();
+      return { key, s3Object, contentType: file.mimetype };
+    } catch (error) {
+      throw new BadRequestException(`File upload failed : ${error}`);
+    }
+  }
+}
