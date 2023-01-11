9a9533751639aed7196b008808fbbb2c28db3546
YuSeokHyun <fkstndnjs@naver.com>
2023-01-11 11:32:32 +0900

    chore: add guard magazine

--- src/magazine/magazine.controller.ts
+++ src/magazine/magazine.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { RolesGuard } from '../auth/role/role.guard';
 import { MagazineService } from './magazine.service';
 
 @Controller('magazine')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class MagazineController {
   constructor(private readonly magazineService: MagazineService) {}
 }
