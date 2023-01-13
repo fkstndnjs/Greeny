d4c09152dcd925abb7a9b357ab30b4251ad80c49
YuSeokHyun <fkstndnjs@naver.com>
2023-01-13 10:27:22 +0900

    chore: add Gurad to event Controller

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { RolesGuard } from '../auth/role/role.guard';
 import { EventService } from './event.service';
 
+@ApiTags('event')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
