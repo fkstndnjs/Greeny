83cbf68f1f1f2624dd9a7a0c2ece265ede31a1db
YuSeokHyun <fkstndnjs@naver.com>
2023-01-16 17:27:19 +0900

    docs: pagination

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,48 @@
+import { IsNumber, IsOptional } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @IsNumber()
+  page: number = 1;
+
+  @IsOptional()
+  @IsNumber()
+  limit: number = 30;
+
+  getOffset(): number {
+    return (this.page - 1) * this.limit || 0;
+  }
+
+  getLimit(): number {
+    return this.limit;
+  }
+}
+
+export class PaginationMeta {
+  constructor(total: number, limit: number, currentPage: number) {
+    this.page = currentPage;
+    this.currentPage = currentPage;
+    this.limit = limit;
+    this.total = total;
+    this.endPage = Math.ceil(total / limit);
+    this.has_previous = currentPage > 1;
+    this.has_next = currentPage < this.endPage;
+  }
+  limit: number;
+  total: number;
+  endPage: number;
+  page: number;
+  currentPage: number;
+  has_previous: boolean;
+  has_next: boolean;
+}
+
+export class Pagination<T> {
+  items: T[];
+  pagination: PaginationMeta;
+
+  constructor(total: number, limit: number, currentPage: number, items: T[]) {
+    this.items = items;
+    this.pagination = new PaginationMeta(total, limit, currentPage);
+  }
+}
