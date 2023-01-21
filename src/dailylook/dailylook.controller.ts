20cea3e05ff994531fc7767121bacfda7432a679
fkstndnjs <fkstndnjs@naver.com>
2023-01-21 15:17:30 +0900

    docs: remove dailylook folde

--- src/dailylook/dailylook.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { DailylookService } from './dailylook.service';
-
-@Controller('dailylook')
-export class DailylookController {
-  constructor(private readonly dailylookService: DailylookService) {}
-}
--- src/dailylook/dailylook.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DailylookService } from './dailylook.service';
-import { DailylookController } from './dailylook.controller';
-
-@Module({
-  controllers: [DailylookController],
-  providers: [DailylookService]
-})
-export class DailylookModule {}
