ba6a29d33b902d0f4399cd3678d6ef43a463a457
fkstndnjs <fkstndnjs@naver.com>
2023-01-21 15:24:54 +0900

    chore: dailyLook Entity

---
+++ src/daily-look/entities/dailyLook.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity } from 'src/common/entity/baseEntity';
+import { Column, Entity } from 'typeorm';
+
+@Entity('dailyLook')
+export class DailyLook extends BaseEntity {
+  @Column({
+    length: 255,
+  })
+  imgUrl: string;
+
+  @Column({
+    length: 100,
+  })
+  title: string;
+
+  @Column({
+    length: 255,
+  })
+  text: string;
+}
