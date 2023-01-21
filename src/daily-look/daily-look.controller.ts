3fe00da4a04351226aa39405690e28d7b7dfb8ce
fkstndnjs <fkstndnjs@naver.com>
2023-01-21 15:30:54 +0900

    chore: dailyLook comment

--- src/daily-look/daily-look.controller.ts
+++ src/daily-look/daily-look.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { DailyLookService } from './daily-look.service';
 
 @Controller('daily-look')
 export class DailyLookController {
   constructor(private readonly dailyLookService: DailyLookService) {}
+
+  // @Post()
+  // async createDailyLook(@Body() body: CreateDailyLookDto){
+  //   await this.dailyLookService.createDailyLook(body);
+  // }
 }
