284926f8691ad692346eed86669a077592c2d4cc
fkstndnjs <fkstndnjs@naver.com>
2023-01-22 21:48:57 +0900

    fix

--- src/daily-look/daily-look.controller.ts
+++ src/daily-look/daily-look.controller.ts
@@ -4,9 +4,4 @@ import { DailyLookService } from './daily-look.service';
 @Controller('daily-look')
 export class DailyLookController {
   constructor(private readonly dailyLookService: DailyLookService) {}
-
-  // @Post()
-  // async createDailyLook(@Body() body: CreateDailyLookDto){
-  //   await this.dailyLookService.createDailyLook(body);
-  // }
 }
