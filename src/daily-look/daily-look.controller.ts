0df8dfc616630e3400f30b146721c7993afced4f
YuSeokHyun <fkstndnjs@naver.com>
2023-02-08 15:39:45 +0900

    feat: create DailyLook, DailyLookTag

--- src/daily-look/daily-look.controller.ts
+++ src/daily-look/daily-look.controller.ts
@@ -1,15 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { RolesGuard } from '../auth/role/role.guard';
+import { Roles } from '../common/decorator/roles';
+import { ApiSuccessResponse } from '../common/decorator/successResponse';
+import { RoleType } from '../common/enum/RoleType';
 import { DailyLookService } from './daily-look.service';
 import { CreateDailyLookDto } from './dto/createDailyLook.dto';
+import { CreateDailyLookTagDto } from './dto/createDailyLookTag.dto';
 
 @ApiTags('데일리룩')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('daily-look')
 export class DailyLookController {
   constructor(private readonly dailyLookService: DailyLookService) {}
 
   @Post()
-  async create(@Body() body: CreateDailyLookDto) {
+  @Roles(RoleType.ADMIN)
+  @ApiSuccessResponse({ paginated: false })
+  async create(@Body() body: CreateDailyLookDto): Promise<void> {
     await this.dailyLookService.create(body);
+  }
+
+  @Post('tag')
+  @Roles(RoleType.ADMIN)
+  @ApiSuccessResponse({ paginated: false })
+  async createTag(@Body() body: CreateDailyLookTagDto): Promise<void> {
+    await this.dailyLookService.createTag(body);
   }
 }
--- src/daily-look/daily-look.service.ts
+++ src/daily-look/daily-look.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  DataSource,
+  EntityNotFoundError,
+  QueryFailedError,
+  Repository,
+} from 'typeorm';
+import { User } from '../user/entities/user.entity';
+import { CreateDailyLookDto } from './dto/createDailyLook.dto';
+import { CreateDailyLookTagDto } from './dto/createDailyLookTag.dto';
+import { DailyLook } from './entities/dailyLook.entity';
+import { DailyLookTag } from './entities/dailyLookTag.entity';
 
 @Injectable()
-export class DailyLookService {}
+export class DailyLookService {
+  constructor(
+    @InjectRepository(DailyLook)
+    private dailyLookRepository: Repository<DailyLook>,
+    @InjectRepository(DailyLookTag)
+    private dailyLookTagRepository: Repository<DailyLookTag>,
+    private dataSource: DataSource,
+  ) {}
+
+  async create(body: CreateDailyLookDto): Promise<void> {
+    const { imgUrl, text, title, dailyLookTag } = body;
+
+    const dailyLookTagFromDb =
+      await this.dailyLookTagRepository.findOneByOrFail({
+        id: Number(dailyLookTag),
+      });
+
+    this.dataSource.transaction(async (manager) => {
+      const dailyLook = new DailyLook();
+
+      dailyLook.imgUrl = imgUrl;
+      dailyLook.text = text;
+      dailyLook.title = title;
+      dailyLook.dailyLookTag = dailyLookTagFromDb;
+
+      await manager.save(dailyLook);
+    });
+  }
+
+  async createTag(body: CreateDailyLookTagDto): Promise<void> {
+    const { name } = body;
+
+    const isDuplicate = this.dailyLookTagRepository.findOneBy({ name });
+    if (isDuplicate) {
+      throw new ConflictException();
+    }
+
+    this.dataSource.transaction(async (manager) => {
+      const dailyLookTag = new DailyLookTag();
+
+      dailyLookTag.name = name;
+
+      await manager.save(dailyLookTag);
+    });
+  }
+}
