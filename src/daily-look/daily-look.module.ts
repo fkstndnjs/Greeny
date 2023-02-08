9e97d73065568602f7d5ebdd64028305fd39210f
YuSeokHyun <fkstndnjs@naver.com>
2023-02-08 10:51:36 +0900

    chore: add TypeOrmModule, AwsModule

--- src/daily-look/daily-look.module.ts
+++ src/daily-look/daily-look.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DailyLookService } from './daily-look.service';
 import { DailyLookController } from './daily-look.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DailyLook } from './entities/dailyLook.entity';
+import { DailyLookTag } from './entities/dailyLookTag.entity';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DailyLook, DailyLookTag, AwsModule])],
   controllers: [DailyLookController],
-  providers: [DailyLookService]
+  providers: [DailyLookService],
 })
 export class DailyLookModule {}
