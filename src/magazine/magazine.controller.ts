f85af5f11f9f5a83f75cfa4874969113f26264c0
fkstndnjs <fkstndnjs@naver.com>
2023-05-18 19:59:09 +0900

    chore: ApiTags, Guard 추가

--- src/magazine/magazine.controller.ts
+++ src/magazine/magazine.controller.ts
@@ -1,14 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { MagazineService } from './magazine.service';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('매거진')
+@UseGuards(JwtAuthGuard)
 @Controller('magazine')
 export class MagazineController {
   constructor(private readonly magazineService: MagazineService) {}
