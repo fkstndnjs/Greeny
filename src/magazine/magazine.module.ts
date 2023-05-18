29d1dd862ea991f27b34c76a43b3c23fbb46c5da
fkstndnjs <fkstndnjs@naver.com>
2023-05-18 23:52:03 +0900

    chore: add SubMagazine to magazine Module

--- src/magazine/magazine.module.ts
+++ src/magazine/magazine.module.ts
@@ -3,9 +3,10 @@ import { MagazineService } from './magazine.service';
 import { MagazineController } from './magazine.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Magazine } from 'src/magazine/entities/magazine.entity';
+import { SubMagazine } from 'src/magazine/entities/subMagazine.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Magazine])],
+  imports: [TypeOrmModule.forFeature([Magazine, SubMagazine])],
   controllers: [MagazineController],
   providers: [MagazineService],
 })
