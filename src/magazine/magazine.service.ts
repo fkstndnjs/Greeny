59afff8c3941461a888e6273447e039a0a091324
fkstndnjs <fkstndnjs@naver.com>
2023-05-19 11:59:09 +0900

    feat: getMagazinez Service

--- src/magazine/magazine.service.ts
+++ src/magazine/magazine.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Pagination, PaginationDto } from 'src/common/dto/pagination.dto';
 import { Magazine } from 'src/magazine/entities/magazine.entity';
 import { Repository } from 'typeorm';
 
@@ -9,4 +10,21 @@ export class MagazineService {
     @InjectRepository(Magazine)
     private magazineRepository: Repository<Magazine>,
   ) {}
+
+  async getMagazines(pagination: PaginationDto): Promise<Pagination<Magazine>> {
+    const [magazines, total] = await this.magazineRepository
+      .createQueryBuilder('magazine')
+      .leftJoin('magazine.subMagazine', 'subMagazine')
+      .orderBy('magazine.createdAt', 'DESC')
+      .skip(pagination.getOffset())
+      .take(pagination.getLimit())
+      .getManyAndCount();
+
+    return new Pagination(
+      total,
+      pagination.getLimit(),
+      pagination.page,
+      magazines,
+    );
+  }
 }
