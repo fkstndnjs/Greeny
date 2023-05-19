0b33f689e9ee82196bfefb9ce65da38d5ac54002
fkstndnjs <fkstndnjs@naver.com>
2023-05-19 10:33:55 +0900

    chore: add ApiProperty to Magazine Entity

--- src/magazine/entities/magazine.entity.ts
+++ src/magazine/entities/magazine.entity.ts
@@ -1,19 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { BaseEntity } from 'src/common/entity/baseEntity';
 import { SubMagazine } from 'src/magazine/entities/subMagazine.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity('magazine')
 export class Magazine extends BaseEntity {
+  @ApiProperty({
+    example:
+      'https://greeny2023.s3.amazonaws.com/event/1675752939340/sample.png',
+  })
   @Column({
     length: 255,
   })
   thumbnail: string;
 
+  @ApiProperty({
+    example: '제목',
+  })
   @Column({
     length: 255,
   })
   title: string;
 
+  @ApiProperty({
+    example: '부제목',
+  })
   @Column({
     length: 255,
   })
