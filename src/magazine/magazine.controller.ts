c8011e8448cec94e9775a8f4358bb6b50e234df8
fkstndnjs <fkstndnjs@naver.com>
2023-05-19 11:45:28 +0900

    feat: create getMagazines

--- src/magazine/magazine.controller.ts
+++ src/magazine/magazine.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { MagazineService } from './magazine.service';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @ApiTags('매거진')
 @UseGuards(JwtAuthGuard)
 @Controller('magazine')
 export class MagazineController {
   constructor(private readonly magazineService: MagazineService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: '매거진 전체 조회',
+  })
+  async getMagazines(@Query() pagination: PaginationDto) {
+    return await this.magazineService.getMagazines(pagination);
+  }
 }
