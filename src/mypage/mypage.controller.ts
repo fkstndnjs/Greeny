d6915d99977e9b5a86f1d9fa6e20bcc56cc03ca2
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 16:12:26 +0900

    chore: add JWT Guard at MyPage

--- src/mypage/mypage.controller.ts
+++ src/mypage/mypage.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { MypageService } from './mypage.service';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @ApiTags('마이페이지')
+@UseGuards(JwtAuthGuard)
 @Controller('mypage')
 export class MypageController {
   constructor(private readonly mypageService: MypageService) {}
