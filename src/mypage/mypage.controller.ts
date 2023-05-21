53ad51dd31d96460c9e46eba140b7a0643e9764e
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 16:11:55 +0900

    chore: add ApiTags at Mypage

--- src/mypage/mypage.controller.ts
+++ src/mypage/mypage.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { MypageService } from './mypage.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('마이페이지')
 @Controller('mypage')
 export class MypageController {
   constructor(private readonly mypageService: MypageService) {}
