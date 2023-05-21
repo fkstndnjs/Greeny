af2d47c4212c85e7b3e3e7b2a6f6a7588a338817
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 15:51:27 +0900

    feat: getOneChallenge endpoint

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorator/currentUser';
@@ -22,5 +22,14 @@ export class ChallengeController {
   async getAllChallenge(
     @CurrentUser() user: User,
     @Query() pagination: PaginationDto,
+  ) {}
+
+  @Get(':idChallenge')
+  @ApiOperation({
+    summary: '챌린지 상세조회',
+  })
+  async getOneChallenge(
+    @CurrentUser() user: User,
+    @Param('idChallenge') idChallenge: number,
   ) {}
 }
