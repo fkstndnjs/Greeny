2479054e4418766a9595c21e6b82459a5382150f
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 15:49:41 +0900

    feat: createChallenge endpoitn

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('challenge')
 export class ChallengeController {
   constructor(private readonly challengeService: ChallengeService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: '챌린지 생성',
+  })
+  async createChallenge() {}
 }
