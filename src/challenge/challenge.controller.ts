c0a8b09c7dab9b571a26e03980e5bcb92fb9042f
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 16:06:46 +0900

    chore: add @ApiTags

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -8,11 +8,12 @@ import {
   Query,
 } from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorator/currentUser';
 import { User } from 'aws-sdk/clients/appstream';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
+@ApiTags('챌린지')
 @Controller('challenge')
 export class ChallengeController {
   constructor(private readonly challengeService: ChallengeService) {}
