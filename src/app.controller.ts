2ecbeefa1dee03d9c2bebe5a01bebc5459b58af2
fkstndnjs <fkstndnjs@naver.com>
2023-05-21 12:07:37 +0900

    chore: remove app controller api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,51 +1,15 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UploadedFile,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/jwt/jwt.guard';
-import { CurrentUser } from './common/decorator/currentUser';
-import { User } from './user/entities/user.entity';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { AwsService } from './aws/aws.service';
 import { Roles } from './common/decorator/roles';
 import { RoleType } from './common/enum/RoleType';
 import { RolesGuard } from './auth/role/role.guard';
 
-@ApiTags('테스트ㄴ')
+@ApiTags('app')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(RoleType.ADMIN)
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly awsService: AwsService,
-  ) {}
-
-  @Get()
-  getHello(@CurrentUser() user: User): string {
-    return this.appService.getHello();
-  }
-
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(@UploadedFile() file: Express.Multer.File) {
-    return await this.awsService.uploadFileToS3('test', file);
-  }
-
-  @Post('delete')
-  async deleteFile(@Body() body: { key: string }) {
-    return this.awsService.deleteS3Object(body.key);
-  }
-
-  @Get('file')
-  async getFile(@Body() body: { key: string }) {
-    return this.awsService.getAwsS3FileUrl(body.key);
-  }
+  constructor(private readonly appService: AppService) {}
 }
