4dbceff43637f3670df72a3dd016fa8b154be3a7
fkstndnjs <fkstndnjs@naver.com>
2023-05-23 23:33:42 +0900

    chore; delete not used module

--- src/daily-look/dto/createDailyLookComment.dto.ts
+++ src/daily-look/dto/createDailyLookComment.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { DailyLookComment } from 'src/daily-look/entities/dailyLookComment.entity';
 
 export class CreateDailyLookCommentDto extends PickType(DailyLookComment, [
